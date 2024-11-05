880e0ea3a119765bbc2196a6d8e0f8600f9ee9d3
Simon Liétar <simon@lietar.net>
2024-11-05 21:04:16 +0100

    Improve precision and recall computation

--- project/metrics.py
+++ project/metrics.py
@@ -4,14 +4,14 @@ import torch
 def compute_metrics(real: torch.Tensor, generated: torch.Tensor, *, k: int):
   def f(test: torch.Tensor, ref: torch.Tensor):
     distances = ((ref[:, None, :] - ref[None, :, :]) ** 2).sum(dim=2)
-    distances_sorted, _ = torch.sort(distances, dim=1)
-    thresholds = distances_sorted[:, k]
+
+    # Using k + 1 because the first element is the distance to itself and it thus zero
+    thresholds = torch.topk(distances, k + 1, dim=1, largest=False).values.max(dim=1).values
 
     distances_test = ((test[:, None, :] - ref[None, :, :]) ** 2).sum(dim=2)
-
     return (distances_test <= thresholds).any(dim=1)
 
   return (
-    f(generated, real).to(torch.float32).mean().item(), # Precision
-    f(real, generated).to(torch.float32).mean().item(), # Recall
+    f(generated, real).to(torch.float32).mean().item(),
+    f(real, generated).to(torch.float32).mean().item(),
   )
