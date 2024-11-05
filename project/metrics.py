c71bbf0f38ac1c2216713137cc2a1482e44a8285
Simon Liétar <simon@lietar.net>
2024-11-05 22:09:15 +0100

    Further improve precision and recall computation

--- project/metrics.py
+++ project/metrics.py
@@ -1,17 +1,19 @@
 import torch
+from sklearn.neighbors import NearestNeighbors
 
 
 def compute_metrics(real: torch.Tensor, generated: torch.Tensor, *, k: int):
   def f(test: torch.Tensor, ref: torch.Tensor):
-    distances = ((ref[:, None, :] - ref[None, :, :]) ** 2).sum(dim=2)
+    neigh = NearestNeighbors(n_neighbors=k)
+    neigh.fit(ref.numpy())
 
-    # Using k + 1 because the first element is the distance to itself and it thus zero
-    thresholds = torch.topk(distances, k + 1, dim=1, largest=False).values.max(dim=1).values
+    distances, _ = neigh.kneighbors()
+    thresholds = torch.as_tensor(distances[:, -1]) ** 2
 
     distances_test = ((test[:, None, :] - ref[None, :, :]) ** 2).sum(dim=2)
-    return (distances_test <= thresholds).any(dim=1)
+    return (distances_test <= thresholds).any(dim=1).to(torch.float32).mean().item()
 
   return (
-    f(generated, real).to(torch.float32).mean().item(),
-    f(real, generated).to(torch.float32).mean().item(),
+    f(generated, real),
+    f(real, generated),
   )
