a6c0f0610f23443838b34e70ae682ee34c99ed96
Simon Liétar <simon@lietar.net>
2024-11-12 13:33:32 +0100

    Add FID metric computation

--- project/metrics.py
+++ project/metrics.py
@@ -1,4 +1,6 @@
 import torch
+import torchvision.datasets
+from torcheval.metrics import FrechetInceptionDistance
 from sklearn.neighbors import NearestNeighbors
 
 
@@ -17,3 +19,34 @@ def compute_metrics(real: torch.Tensor, generated: torch.Tensor, *, k: int):
     f(generated, real),
     f(real, generated),
   )
+
+
+# def compute_fid(real: torch.Tensor, generated: torch.Tensor):
+#   fid = FrechetInceptionDistance()
+#   fid.update(real, True)
+#   fid.update(generated, False)
+
+#   return fid.compute()
+
+
+def compute_fid(real_data: torch.Tensor):
+  transform = torchvision.transforms.Compose([
+      torchvision.transforms.ToTensor(),
+      torchvision.transforms.Normalize(mean=0.5, std=0.5),
+  ])
+
+  dataset = torchvision.datasets.MNIST(root='data/MNIST/', train=False, transform=transform, download=False)
+  batch_size = 100
+
+  real_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
+  generated_loader = torch.utils.data.DataLoader(dataset=torch.utils.data.TensorDataset(real_data), batch_size=batch_size, shuffle=False)
+
+  fid = FrechetInceptionDistance()
+
+  for real in real_loader:
+    fid.update(real, True)
+
+  for generated in generated_loader:
+    fid.update(generated, False)
+
+  return fid.compute()
